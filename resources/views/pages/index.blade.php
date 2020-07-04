bd4b0167be56242439bf303096644ef095eb5bc1
Lucas van Beek <info@lucasvanbeek.nl>
2020-07-04 23:15:43 +0200

    A11y

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -52,7 +52,7 @@
         <div class="container px-5 py-24 mx-auto">
             <div class="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto">
                 <h1 class="flex-grow sm:pr-16 text-2xl font-medium title-font text-white">Wil je komen spelen? Join dan ook onze Discord!</h1>
-                <a class="flex-shrink-0 text-white bg-orange-500 border-0 py-2 px-8 focus:outline-none hover:bg-orange-600 rounded text-lg mt-10 sm:mt-0" href="https://discord.gg/8bGrXvz">Discord</a>
+                <a class="flex-shrink-0 text-white bg-orange-700 border-0 py-2 px-8 focus:outline-none hover:bg-orange-600 rounded text-lg mt-10 sm:mt-0" href="https://discord.gg/8bGrXvz">Discord</a>
             </div>
         </div>
     </section>
